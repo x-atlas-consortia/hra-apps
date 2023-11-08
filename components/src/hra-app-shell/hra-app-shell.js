c59622aeb0f01e4cfbaa84e6ca454bdd05ea4e15
Bruce W. Herr II <bherr@indiana.edu>
2023-11-08 16:16:50 -0500

    Revert back to using shadow dom

--- components/src/hra-app-shell/hra-app-shell.js
+++ components/src/hra-app-shell/hra-app-shell.js
@@ -1,24 +1,30 @@
 import cssString from './hra-app-shell.css';
 import templateString from './hra-app-shell.html';
 
+const globalStyles = document.createElement('template');
+globalStyles.innerHTML = `<style>${cssString}</style>`;
+
+const template = document.createElement('template');
+template.innerHTML = templateString;
+
 // Apply styles globally (normally not good for web components, but necessary for a full app shell)
-document.addEventListener('DOMContentLoaded', async () => {
-  const globalStyles = document.createElement('template');
-  globalStyles.innerHTML = `<style>${cssString}</style>`;
+document.addEventListener('DOMContentLoaded', async () => {  
   document.head.appendChild(globalStyles.content.cloneNode(true));
+  await Promise.allSettled([customElements.whenDefined('hra-app-shell')]);
+  document.body.style.visibility = 'visible';
 });
 
 class HraAppShell extends HTMLElement {
-  connectedCallback() {
-    setTimeout(() => {
-      this.innerHTML = templateString.replace('<slot></slot>', this.innerHTML);
-      document.body.style.visibility = 'visible';
+  constructor() {
+    super();
 
-      const root = this.getRootNode();
-      root.getElementById('logo').addEventListener('click', () => {
-        location.href = '/';
-      });
-    }, 100);
+    this.attachShadow({ mode: 'open' });
+    this.shadowRoot.appendChild(template.content.cloneNode(true));
+    this.shadowRoot.appendChild(globalStyles.content.cloneNode(true));
+
+    this.shadowRoot.getElementById('logo').addEventListener('click', () => {
+      location.href='/';
+    });
   }
 }
 
