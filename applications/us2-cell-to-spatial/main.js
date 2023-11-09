e3d1b459e97d212a7c9ffed5077d74489e61ba7b
Bruce W. Herr II <bherr@indiana.edu>
2023-11-09 16:32:12 -0500

    US2 logic mostly in place (needs styling and a little clean up)

--- applications/us2-cell-to-spatial/main.js
+++ applications/us2-cell-to-spatial/main.js
@@ -1,4 +1,5 @@
 import { TabulatorFull } from 'tabulator-tables';
+import sample from './heart-cell-summary.csv';
 
 async function getSimilarHraItems(csvString) {
   return fetch('https://apps.humanatlas.io/api/ctpop/cell-summary-rui-location', {
@@ -10,30 +11,68 @@ async function getSimilarHraItems(csvString) {
   }).then((r) => r.json());
 }
 
-window.addEventListener('DOMContentLoaded', async () => {
-  const csvInput = document.getElementById('csv-input');
-  const submitBtn = document.getElementById('submit-file');
-  const results = document.getElementById('results');
+let csvString;
+let generatedRuiLocations;
 
+window.addEventListener('DOMContentLoaded', async () => {
+  const useSample = document.getElementById('use-sample');
+  useSample.addEventListener('click', async () => {
+    setCsvFile(sample, 'heart-cell-summary.csv');
+  });
+
+  const submitBtn = document.getElementById('submit-file');
   submitBtn.addEventListener('click', async () => {
-    results.style.display = 'none';
-    submitBtn.disabled = true;
-    if (csvInput.files.length > 0) {
-      const csvString = await csvInput.files[0].text();
+    clearResults();
+    if (csvString) {
       const { sources, rui_locations } = await getSimilarHraItems(csvString);
 
       if (sources?.length > 0) {
+        results.style.display = 'block';
         updateAsTable(sources);
         updateDatasetsTable(sources);
         updateEui(rui_locations);
-        results.style.display = 'block';
       } else {
         alert('Bad Data');
       }
     }
-    submitBtn.disabled = false;
+  });
+
+  const removeBtn = document.getElementById('remove-file');
+  removeBtn.addEventListener('click', async () => {
+    if (csvString) {
+      clearResults();
+      setCsvFile(undefined);
+    }
+  });
+
+  const fileInput = document.getElementById('file-input');
+  fileInput.addEventListener('change', async () => {
+    if (fileInput.files.length > 0) {
+      const csvString = await fileInput.files[0].text();
+      const fileName = fileInput.files[0].name;
+      setCsvFile(csvString, fileName);
+    }
   });
 });
+
+function setCsvFile(newCsvString, fileName) {
+  csvString = newCsvString ? newCsvString : undefined;
+  
+  const status = document.getElementById('upload-status');
+  status.innerHTML = newCsvString ? fileName : 'No file loaded';
+
+  const submitBtn = document.getElementById('submit-file');
+  submitBtn.style.display = !!csvString ? 'block' : 'none';
+
+  const removeBtn = document.getElementById('remove-file');
+  removeBtn.style.display = !!csvString ? 'block' : 'none';
+
+  const useSample = document.getElementById('use-sample');
+  useSample.style.display = !!csvString ? 'none' : 'block';
+
+  const uploadFile = document.getElementById('upload-file');
+  uploadFile.style.display = !!csvString ? 'none' : 'block';
+}
 
 let asTable;
 let datasetTable;
@@ -76,9 +115,15 @@ function updateDatasetsTable(sources) {
   button2.addEventListener('click', () => datasetTable.download('csv', 'similar-atlas-datasets.csv'));
 }
 
+function clearResults() {
+  const results = document.getElementById('results');
+  results.style.display = 'none';
+}
+
 function updateEui(rui_locations) {
   // Construct EUI
   let eui = document.getElementsByTagName('ccf-eui')[0];
+  generatedRuiLocations = rui_locations;
   if (!eui) {
     eui = document.createElement('ccf-eui');
     eui.setAttribute('use-remote-api', 'false');
@@ -86,6 +131,9 @@ function updateEui(rui_locations) {
     eui.setAttribute('login-disabled', 'true');
     const container = document.getElementById('eui-container');
     container.appendChild(eui);
+
+    const button = document.getElementById('download-as-jsonld');
+    button.addEventListener('click', () => alert(JSON.stringify(rui_locations, null, 2)));
   }
 
   eui.setAttribute('data-sources', JSON.stringify([JSON.stringify(rui_locations)]));
